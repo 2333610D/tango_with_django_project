74c2b263be73dc04756d4a00c6c30275e367318b
Greg Dawson <2333610d@student.gla.ac.uk>
2019-02-05 23:35:05 +0000

    end of ch6

--- populate_rango.py
+++ populate_rango.py
@@ -14,18 +14,18 @@ def populate():
 # through each data structure, and add the data to our models.
 
     python_pages = [
-        {"title": "Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/"},
-        {"title":"How to Think like a Computer Scientist", "url":"http://www.greenteapress.com/thinkpython/"},
-        {"title":"Learn Python in 10 Minutes", "url":"http://www.korokithakis.net/tutorials/python/"} ]
+        {"title": "Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/", "views": 10},
+        {"title":"How to Think like a Computer Scientist", "url":"http://www.greenteapress.com/thinkpython/", "views": 7},
+        {"title":"Learn Python in 10 Minutes", "url":"http://www.korokithakis.net/tutorials/python/", "views": 4} ]
 
     django_pages = [
-        {"title":"Official Django Tutorial", "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
-        {"title":"Django Rocks", "url":"http://www.djangorocks.com/"},
-        {"title":"How to Tango with Django", "url":"http://www.tangowithdjango.com/"} ]
+        {"title":"Official Django Tutorial", "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 8},
+        {"title":"Django Rocks", "url":"http://www.djangorocks.com/", "views":2},
+        {"title":"How to Tango with Django", "url":"http://www.tangowithdjango.com/", "views":3} ]
 
     other_pages = [
-        {"title":"Bottle", "url":"http://bottlepy.org/docs/dev/"},
-        {"title":"Flask", "url":"http://flask.pocoo.org"} ]
+        {"title":"Bottle", "url":"http://bottlepy.org/docs/dev/", "views":9},
+        {"title":"Flask", "url":"http://flask.pocoo.org", "views":6} ]
 
     cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64}, "Django": {"pages": django_pages, "views":64, "likes":32}, "Other Frameworks": {"pages": other_pages, "views":32, "likes":16} }
 
@@ -41,7 +41,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cats[cat]["views"], cats[cat]["likes"])
         for p in cat_data["pages"]:        
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
             
 # Print out the categories we have added.
 
@@ -49,7 +49,7 @@ def populate():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
